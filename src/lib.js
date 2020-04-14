a3284a6421d81e053f72830adc82cc45907807a2
wsa <dannvx@gmail.com>
2020-04-14 10:00:02 +0300

    cleanup

--- src/lib.js
+++ src/lib.js
@@ -1,22 +1,5 @@
 const {log, logG, logger} = require("../tools/logger").logger()
 
-class Pair extends Array {
-  constructor(l, r) {
-    super(l, r)
-  }
-}
-
-class Pairs extends Array {
-  constructor(...pairs) {
-    pairs.forEach((pair) => {if (pair instanceof Pair) throw new Error()})
-    super(...pairs)
-  }
-}
-
-function* CartesianProduct(arr) {
-
-}
-
 function doRecursivelyIterate(data, depth, noRepeat, eachCb, doRecursivelyIterate) {
   // console.log(`depth: ${depth}`)
 
@@ -57,16 +40,59 @@ function recursivelyIterate(data, noRepeat) {
   // }, recursivelyIterate)
 }
 
+class Pair extends Array {
+  constructor(l, r) {
+    super(l, r)
+  }
+}
+
+class Pairs extends Array {
+  constructor(...pairs) {
+    pairs.forEach((pair) => {if (pair instanceof Pair) throw new Error()})
+    super(...pairs)
+  }
+}
+
+function* CartesianProduct(arr) {
+
 }
 
 function cartesianProductSync(arr) {
+  // const pairs = new Pairs()
+  //
+  // arr.forEach(left => arr.forEach(
+  //   right => pairs.push(new Pair(left, right)))
+  // )
+  //
+  // return pairs
+
   const pairs = new Pairs()
+  doRecursivelyIterate(arr, 1, null, (v) => {
+    pairs.push(new Pair(v[0], v[1]))
+  }, doRecursivelyIterate)
 
-  arr.forEach(left => arr.forEach(
-    right => pairs.push(new Pair(left, right)))
-  )
+  return Pairs
+}
 
-  return pairs
+function* CartesianProduct(arr) {
+
+}
+
+function permutations(arr) {
+
+}
+
+// function getTrails() {}
+
+// get all possible paths
+function paths(edges) {
+
+}
+
+// create a graph from given vertices, wherein every vertice is
+// adjacent to every other vertice
+function connectAll(vertices) {
+  return cartesianProductSync(vertices)
 }
 
 module.exports = {
