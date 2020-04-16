d8a67f09e08d7e657d03570183ef0aa85ce895e5
wsa <dannvx@gmail.com>
2020-04-16 20:21:48 +0300

    add an example of makeRecursivelyIterate usage

--- demo/demo.js
+++ demo/demo.js
@@ -14,6 +14,34 @@ function useMakeRecursivelyIterate() {
   return p
 }
 
+function exampleMakeRecursivelyIterate() {
+  const edges = []
+  const makeEdges = makeRecursivelyIterate((v, data, depth, eachCb) => {
+    eachCb((v) ? [data].concat(v) : [data])
+  })
+
+  makeEdges([0,1,2], 1, true, (v) => {
+    edges.push(v)
+  }, makeEdges)
+
+  console.log(edges);
+
+
+  const trails = []
+  const makeTrails = makeRecursivelyIterate((v, data, depth, eachCb) => {
+    if (!v) return eachCb([data])
+
+    console.log((v[0][0] === data[1]), `${data}-${v[0]}`);
+    if (v[0][0] === data[1]) eachCb([data].concat(v))
+  })
+
+  makeTrails(edges, edges.length-1, true, (v) => {
+    console.log(v);
+    trails.push(v)
+  }, makeTrails)
+  return trails
+}
+
 /*
 function useMakeRecursivelyIterate() {
   const p = []
@@ -32,5 +60,5 @@ function useMakeRecursivelyIterate() {
 */
 
 module.exports = {
-  useMakeRecursivelyIterate
+  useMakeRecursivelyIterate, exampleMakeRecursivelyIterate
 }
